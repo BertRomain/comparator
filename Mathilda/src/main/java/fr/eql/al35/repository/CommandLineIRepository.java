14a4b196f4409d65c97a3ffe0f9102767d5447d6
BertRomain <r.bertoux@free.Fr>
2021-07-15 20:57:36 +0200

    +suppression éléments inutiles

--- Mathilda/src/main/java/fr/eql/al35/repository/CommandLineIRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.eql.al35.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.eql.al35.entity.CommandLine;
-
-public interface CommandLineIRepository extends CrudRepository<CommandLine, Integer> {
-
-}
